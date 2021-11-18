cd11c1d440b1fdec22aad055dbdfde416302b4b5
Hailey Thompson <haileythompson905@yahoo.com>
2021-11-17 19:33:43 -0700

    added notes on var and transfer

---
+++ notes.js
@@ -0,0 +1,42 @@
+// Datatypes
+// string "" ''
+// Boolean true false
+// integer 123123 -123123
+// float 234.2343
+// Hash { first_name: "bob", age: 2}
+// Array [ '', 324343]
+// null
+// NaN
+// undefined
+// + - / * %
+// ()
+// || &&
+// < > <= >=
+// =
+// ==
+// ===
+// !=
+// // !==
+// ++
+// --
+// +=
+// -=
+// *=
+// variables
+// var first_name = 'bob'
+// - letters, number, _ $
+// - has to begin with letter $ _
+// - case sensitive
+// var First_name = 'bob'- Javascript treats this as two separate form the other above
+// - stay away from reserve words
+// -camelcase example below:
+// var FirstName ="bob"
+// no longer using var because of scope 
+// var- file wide scope in most cases 
+// use these instead:
+// let
+// const
+// scope is where ever it  is you define it in.
+// let- things you want to change 
+// const - constant or things you don't want changed
+// scope is where ever it is you
